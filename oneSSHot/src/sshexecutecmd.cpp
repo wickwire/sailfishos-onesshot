fe65c38b2ae373f69d81960cd7d2ef9d7dc7a167
wickwire <wickwired@gmail.com>
2014-01-26 21:47:20 +0000

    working on ssh commands QProcess - detached is a good thing! ssh udp like actions!

--- oneSSHot/src/sshexecutecmd.cpp
+++ oneSSHot/src/sshexecutecmd.cpp
@@ -9,10 +9,20 @@ sshExecuteCmd::sshExecuteCmd(QObject *parent) :
 
 void sshExecuteCmd::executeSSH(){
 
+    QString username, host, port, command;
+
+    username="wickwire";
+    host="192.168.55.100";
+    port="22";
+    command="echo `date +'%Y-%m-%d %H:%M:%S'` > /home/wickwire/cenas.txt";
+
     QProcess *proc = new QProcess();
 
-    proc->start("ssh wickwire@192.168.55.100 echo `date +'%Y-%m-%d %H:%M:%S'` > /home/wickwire/cenas.txt");
-    proc->waitForFinished();
+    //proc->start("ssh wickwire@192.168.55.100 -p 22 echo `date +'%Y-%m-%d %H:%M:%S'` > /home/wickwire/cenas.txt");
+    //proc->waitForFinished();
+
+    //proc->startDetached("ssh wickwire@192.168.55.100 -p 22 echo `date +'%Y-%m-%d %H:%M:%S'` > /home/wickwire/cenas.txt");
+    proc->startDetached("ssh " + username + "@" + host + " -p " + port + " " + command);
 
     QString result=proc->readAllStandardOutput();
     qDebug() << "cmd result: " + result;
